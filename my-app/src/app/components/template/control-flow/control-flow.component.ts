ca13dd079980bfeebfeb1b2fb5e01e93ac6c0b64
Bruno <bbnsdevelop@gmail.com>
2025-08-12 22:00:12 -0300

    control flow switch case

--- my-app/src/app/components/template/control-flow/control-flow.component.ts
+++ my-app/src/app/components/template/control-flow/control-flow.component.ts
@@ -22,22 +22,22 @@ export class ControlFlowComponent {
 
 
     people = [
-      { name: 'John Doe', age: 25 },
-      { name: 'Jane Smith', age: 17 },
-      { name: 'Michael Johnson', age: 32 },
-      { name: 'Emily Davis', age: 19 },
-      { name: 'David Wilson', age: 45 },
-      { name: 'Sarah Brown', age: 16 },
-      { name: 'Chris Miller', age: 28 },
-      { name: 'Ashley Garcia', age: 21 },
-      { name: 'Robert Taylor', age: 39 },
-      { name: 'Jessica Martinez', age: 15 }
+      { name: 'John Doe', age: 25, gender: 'M' },
+      { name: 'Jane Smith', age: 17, gender: 'F' },
+      { name: 'Michael Johnson', age: 32, gender: 'M' },
+      { name: 'Emily Davis', age: 19, gender: 'F' },
+      { name: 'David Wilson', age: 45, gender: 'M' },
+      { name: 'Sarah Brown', age: 16, gender: 'F' },
+      { name: 'Chris Miller', age: 28, gender: 'F' },
+      { name: 'Ashley Garcia', age: 21, gender: 'F' },
+      { name: 'Robert Taylor', age: 39, gender: 'M' },
+      { name: 'Jessica Martinez', age: 15, gender: 'F' }
     ];
 
     people2: Person[] = [];
 
     public addPerson(name: string, age: string): void {
-      this.people.push({ name: name, age: parseInt(age) });
+      this.people.push({ name: name, age: parseInt(age), gender: 'M' });
       this.people2.push(new Person(name, parseInt(age)));
     }
   }
